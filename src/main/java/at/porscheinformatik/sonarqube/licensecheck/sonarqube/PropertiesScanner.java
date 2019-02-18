5ead0ebbbc8b6bb83a5bd17b093cc08651a41971
Matjaž Domen Pečan <matjaz.pecan@affinitas.de>
2019-02-19 00:27:18 +0100

    Change to lang3.

--- src/main/java/at/porscheinformatik/sonarqube/licensecheck/sonarqube/PropertiesScanner.java
+++ src/main/java/at/porscheinformatik/sonarqube/licensecheck/sonarqube/PropertiesScanner.java
@@ -1,6 +1,6 @@
 package at.porscheinformatik.sonarqube.licensecheck.sonarqube;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
 import org.sonar.api.config.Configuration;
